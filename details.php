0d046e7090972c47a9cf3e4d8601eb59cd1e7c2a
Mst Rinky Akter <101060713+mstrinkyakter@users.noreply.github.com>
2023-11-07 17:10:45 +0600

    Add files via upload
    blog-site

--- details.php
+++ details.php
@@ -1,60 +1,44 @@
-<?php 
-require 'config.php';
-$query ="SELECT * FROM post";
-$stmt=$pdo->prepare($query);
-$stmt->execute();
-$posts=$stmt->fetchAll(PDO::FETCH_ASSOC);
-// foreach ($posts as $post){
-//     echo "<p> {$post['Title']} </p>";
-// }
+<?php
+include "includes/header.php";
 ?>
-
-
-<?php include "includes/header.php"; ?>
-    <main>
+<main>
         <section class="py-5">
             <div class="container">
-                <div class="post g-5">
-                    <div class="row">
+                <div class="row g-5">
                     <article class="col-12 col-lg-8">
                         <h1 class="text-center">Featured Posts</h1>
-                        <div class="post gy-4">
-                            <?php foreach($posts as $post): ?>
+                        <div class="row gy-4">
                             <div class="col-12">
                                 <div class="card">
                                     <img src="https://img.freepik.com/free-vector/online-tutorials-concept_52683-37480.jpg?size=626&ext=jpg&ga=GA1.1.1413502914.1697414400&semt=ais" alt=""
                                         height="400px" width="100%px">
                                     <div class="card-body">
-                                        <h3> <?php echo $post['Title']; ?></h3>
-                                        <p> <?php echo $post['Description']; ?></p>
-                                        <a href="details.php">Read More...</a>
+                                        <h3>Lorem ipsum dolor sit.</h3>
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas ducimus corrupti odio repellendus. Ullam numquam dolores asperiores iure. Maxime, ipsam? Perferendis quod sint, nobis quas dolorum eligendi debitis delectus fuga inventore dolores corporis minus similique veritatis magni omnis pariatur beatae repudiandae hic nemo magnam nulla nam a maxime? Sit cumque suscipit fugiat deserunt natus exercitationem voluptate libero odio fuga alias corporis porro rerum fugit repudiandae doloremque voluptatum magni, aspernatur culpa perferendis hic nisi! Harum, distinctio magni? Voluptates laboriosam impedit fugiat maxime id non natus dolorem quibusdam odit fugit! Eveniet, neque quis odio provident dicta veritatis aliquid exercitationem rerum libero fugiat amet maiores minus ducimus atque nam illum nulla pariatur doloremque ad modi cum temporibus dolorum totam quod? Accusantium labore esse iusto, nobis velit veniam explicabo et autem earum delectus quo! Expedita fuga tempore eos deleniti! Totam suscipit deserunt obcaecati reprehenderit alias tempore sequi, expedita assumenda similique. Incidunt tempore vitae dolore, corporis porro natus officia voluptatibus excepturi suscipit repellat commodi, ut similique nihil reiciendis eius non esse eum earum rerum deserunt qui labore ullam molestias facilis. Consequatur saepe, itaque non, eos deserunt dolorem corrupti, tempore ut soluta veritatis nisi nemo cum nulla minima ipsa possimus dignissimos repellat nobis placeat animi ex!</p>
+                                        
                                     </div>
                                 </div>
                             </div>
                            
-                            <?php endforeach; ?>
                             
                         </div>
                     </article>
                     <aside class="col-12 col-lg-4">
-                        <div class="post g-4">
+                        <div class="row g-4">
                             <div class="col-12 ">
                               <h3 class="mt-4">Featured Categories</h3>
-                               
-                                    <ul class="list-group list-group-flush list">
+                              <ul class="list-group list-group-flush list">
                                         <li class="list-group-item"><a href="">Web Design & Development</a></li>
                                         <li class="list-group-item"><a href="">Graphic & Multimedia</a></li>
                                         <li class="list-group-item"><a href="">Software Development</a></li>
                                         <li class="list-group-item"><a href="">Networking</a></li>
                                         <li class="list-group-item"><a href="">Freelancing & Digital Marketing</a></li>
                                     </ul>
-                                
                             </div>
+                           
                         </div>
 
                     </aside>
-                    </div>
-                   
                 </div>
                 <br>
                 <!-- pagination  -->
@@ -81,4 +65,4 @@ $posts=$stmt->fetchAll(PDO::FETCH_ASSOC);
 
         </section>
     </main>
-<?php include "includes/footer.php" ?>
+    <?php include "includes/footer.php"; ?>
